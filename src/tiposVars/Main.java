dfb24dfc48e8f63927aac8e569592211110b038b
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-06-29 22:24:01 -0300

    tipos em java

---
+++ src/tiposVars/Main.java
@@ -0,0 +1,24 @@
+package tiposVars;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//  Sintaxe:
+		// <tipo> <nome> = <valor>
+		int idade = 19;
+		double altura = 1.88;
+		char sexo = 'M';
+		
+		// Uma variável possui:
+		// Nome
+		// Tipo
+		// Valor
+		// Endereço ** 
+		
+		System.out.println("Nome: João Victor");
+		System.out.println("Idade: " + idade);
+		System.out.println("Altura: " + altura);
+		System.out.println("Sexo: " + sexo);
+	}
+
+}
