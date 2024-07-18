5f4b11141e560471d651e13d1b27930b48691e7e
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-07-18 19:52:32 -0300

    triangulos com POO

---
+++ src/modulo_8/trianguloPOO/Triangle.java
@@ -0,0 +1,19 @@
+package modulo_8.trianguloPOO;
+
+public class Triangle {
+	
+	public double a;
+	public double b;
+	public double c;
+
+// Em suma,
+// CLASSSE É UM TIPO ESTRUTURADO QUE PODE CONTER:
+// - Atributos
+// - Métodos
+
+// A Classe também pode prover muitos outros recursos, tais como:
+// - Construtores
+// - Sobrecarga
+// - Encapsulamento
+// - Herança
+}
