5657dabead9de83441fa2bffaadd316c32994f92
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-08-12 20:25:36 -0300

    garbage collector

--- src/modulo_9/introducao/main.java
+++ src/modulo_9/introducao/main.java
@@ -24,6 +24,26 @@ public class main {
 		// "y recebe uma CÓPIA de x"
 		y=y+5;
 		System.out.println(x); // x segue inalterado
+		
+		
+		// GARBAGE COLLECTOR
+		// objetos uma vez instanciados e que perderam a referência serão desalocados da memória heap (memória dinâmica) pelo garbage collector.
+		Product p4 = new Product("Caderno", 20.00, 5);
+		Product p5 = new Product("Caneta", 2.00, 5);
+		p4 = p5; // O apontamento do caderno sumiu do mapa... Foi devorado pelo garbage collector.
+		
+		// Observação: Desalocação por escoppo não é garbage collector. (VEJA  method 1)
+		
+		
 	}
+	
+	void method1() {
+		int x = 10;
+		if(x>10) {
+			int y=5;
+		}
+		// y só existe no escopo do if
+		System.out.println(x);
+	};
 
 }
